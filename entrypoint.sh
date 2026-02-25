de5eb95eca23932d2697b4154dfa97e03f4431c6
shahram7 <34509793+shahram7@users.noreply.github.com>
2026-02-25 12:09:26 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,3 +17,6 @@ if [ -n "$EXERCISM_TOKEN" ] && [ -n "$EXERCISM_WORKSPACE" ]; then
 else
     echo "⚠️ Exercism token or workspace not provided, skipping Exercism configuration."
 fi
+
+# --- Start OpenVSCode Server ---
+exec /home/.openvscode-server/bin/openvscode-server "$@"
